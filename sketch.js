8af5dd84baa8796bf2361df63b06dff74d70aac4
jasonKuzhitharayil9192007 <72137271+jasonKuzhitharayil9192007@users.noreply.github.com>
2020-12-21 20:18:12 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,16 @@
+// creating supply mission 2.
 var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
-var packageBody,ground
+var packageBody,ground,box,leftS,rightS;
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
+// preloading the images
 function preload()
 {
-	helicopterIMG=loadImage("helicopter.png")
-	packageIMG=loadImage("package.png")
+	helicopterIMG=loadImage("helicopter.png");
+	packageIMG=loadImage("package.png");
 }
 
 function setup() {
@@ -17,16 +19,22 @@ function setup() {
 	
 
 	packageSprite=createSprite(width/2, 80, 10,10);
-	packageSprite.addImage(packageIMG)
-	packageSprite.scale=0.2
+	packageSprite.addImage(packageIMG);
+	packageSprite.scale=0.2;
 
 	helicopterSprite=createSprite(width/2, 200, 10,10);
-	helicopterSprite.addImage(helicopterIMG)
-	helicopterSprite.scale=0.6
-
+	helicopterSprite.addImage(helicopterIMG);
+	helicopterSprite.scale=0.6;
+   
+	// creating a ground
 	groundSprite=createSprite(width/2, height-35, width,10);
-	groundSprite.shapeColor=color(255)
+	groundSprite.shapeColor=color(255);
+   
+	// creating the box
+	box = createSprite(400,620,200,20);
 
+	leftS = createSprite(300,580,20,100);	
+	rightS = createSprite(500,580,20,100);
 
 	engine = Engine.create();
 	world = engine.world;
@@ -35,9 +43,13 @@ function setup() {
 	World.add(world, packageBody);
 	
 
-	//Create a Ground
+	// also creating a ground
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
- 	World.add(world, ground);
+	 World.add(world, ground);
+	 
+	 	//also creating the box
+	box = Bodies.rectangle(400,620,200,20, {isStatic:true} );
+	World.add(world, box);
 
 
 	Engine.run(engine);
@@ -49,7 +61,8 @@ function draw() {
   rectMode(CENTER);
   background(0);
   packageSprite.x= packageBody.position.x 
-  packageSprite.y= packageBody.position.y 
+  packageSprite.y= packageBody.position.y
+
   drawSprites();
  
 }
@@ -63,3 +76,4 @@ Matter.Body.setStatic(packageBody,false);
 
 
 
+// thank you for looking at my project :)
